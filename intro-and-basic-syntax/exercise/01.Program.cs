2232fa35b9a942d6e02b750a058793c22fe22cc5
rebown <kiril270405@gmail.com>
2018-03-14 22:05:27 +0200

    Added comments

--- intro-and-basic-syntax/exercise/01.Program.cs
+++ intro-and-basic-syntax/exercise/01.Program.cs
@@ -9,11 +9,13 @@ namespace DebitCardNumber
     class Program
     {
         static void Main(string[] args)
-        {
+        {	
+			//First we parse the text from the console to int
             int n1 = int.Parse(Console.ReadLine());
             int n2 = int.Parse(Console.ReadLine());
             int n3 = int.Parse(Console.ReadLine());
             int n4 = int.Parse(Console.ReadLine());
+			//Second we use placeholders and print
             Console.WriteLine("{0, 0:D4} {1, 0:D4} {2, 0:D4} {3, 0:D4}", n1, n2, n3, n4 );
         }
     }
