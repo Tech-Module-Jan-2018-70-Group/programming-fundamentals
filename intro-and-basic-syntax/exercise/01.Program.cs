b4e7d44711531db527ae8353abc0979ef6bd95a2
@TodorNikolov89 <tod_ant@yahoo.com>
2018-03-14 22:01:13 +0200

    Added comments to the source files

--- intro-and-basic-syntax/exercise/01.Program.cs
+++ intro-and-basic-syntax/exercise/01.Program.cs
@@ -10,10 +10,12 @@ namespace DebitCardNumber
     {
         static void Main(string[] args)
         {
+            // On the next four lines, we are reading integer numbers from the console
             int n1 = int.Parse(Console.ReadLine());
             int n2 = int.Parse(Console.ReadLine());
             int n3 = int.Parse(Console.ReadLine());
             int n4 = int.Parse(Console.ReadLine());
+            // We print the result after proper formatting
             Console.WriteLine("{0, 0:D4} {1, 0:D4} {2, 0:D4} {3, 0:D4}", n1, n2, n3, n4 );
         }
     }
--- intro-and-basic-syntax/exercise/04.Beverage Labelss.cs
+++ intro-and-basic-syntax/exercise/04.Beverage Labelss.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +9,18 @@ namespace _04.Beverage_Labelss
     {
         static void Main(string[] args)
         {
-
-            string ivan = Console.ReadLine();
-
-            Console.WriteLine(ivan);
-
+            // On the first line we read the name of the Beverage from the Console
+            string name = Console.ReadLine();
+            // On the next three lines we read volume, energy and sugar of teh Beverage per 100 ml
+            int volume = int.Parse(Console.ReadLine()); // per 100ml
+            int energy = int.Parse(Console.ReadLine());  // per 100ml
+            int sugar = int.Parse(Console.ReadLine()); //  per 100ml
+            // We calculate the energy
+            double conEnergy = (energy * volume) / 100.00;
+            // We calculate the sugar
+            double conSugar = (sugar * volume) / 100.00;
+            // Next line prints the result
+            Console.WriteLine("{0}ml {1}:\n{2}kcal, {3}g sugars", volume, name, conEnergy, conSugar);
         }
     }
 }
