2d6fc48929a724f58a7e75c4b3acb11be54806fb
@TodorNikolov89 <tod_ant@yahoo.com>
2018-03-14 19:41:00 +0200

    Added CharacterStats and MilesToKilometters

---
+++ intro-and-basic-syntax/exercise/CharacterStats.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЦхарацтерСтатс
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int health = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+            int energy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Health: |" + new string('|', health) + new string('.',maxHealth - health) + "|");
+            Console.WriteLine("Energy: |" + new string('|', energy) + new string('.', maxEnergy - energy) + "|");
+        }
+    }
+}
---
+++ intro-and-basic-syntax/exercise/MilesToKilometters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilesToKilometters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double miles = double.Parse(Console.ReadLine());
+            double kilometters = miles * 1.60934;
+            Console.WriteLine("{0:f2}", kilometters);
+        }
+    }
+}
